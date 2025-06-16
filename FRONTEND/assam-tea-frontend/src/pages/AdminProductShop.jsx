5350310a20f01b122d9f0c487630397395b6d58b
KamaniKrish <kamanikrish56@gmail.com>
2025-06-16 12:36:44 +0530

    Add files via upload

---
+++ FRONTEND/assam-tea-frontend/src/pages/AdminProductShop.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeroShop from '../components/AdminHeroShop';
+import ProductManager from '../components/AdminProductManager';
+
+const AdminShopPage = () => {
+  return (
+    <>
+      <HeroShop />
+      <ProductManager />
+    </>
+  );  
+};
+
+export default AdminShopPage;
