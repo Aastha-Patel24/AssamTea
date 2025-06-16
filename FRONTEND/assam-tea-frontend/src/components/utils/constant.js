e7e71600fb82dac2b22c48b34465efd4bebe8af8
Aastha-Patel24 <aasthapatel.ce@gmail.com>
2025-06-16 18:34:51 +0530

    Delete FRONTEND/assam-tea-frontend directory

--- FRONTEND/assam-tea-frontend/src/components/utils/constant.js
+++
@@ -1,5 +0,0 @@
-export const PRODUCT_API_END_POINT="http://localhost:5000/api/product"
-export const USER_API_END_POINT="http://localhost:5000/api/user"
-export const CART_API_END_POINT="http://localhost:5000/api/cart"
-export const ORDER_API_END_POINT="http://localhost:5000/api/order"
-
